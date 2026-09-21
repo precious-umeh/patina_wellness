d2ec59369bd0f8b63b147d40ca98c192f42b8244
Precious Umeh <preciouschukwuanugoumeh@gmail.com>
2026-09-21 08:54:54 +0100

    add FloatingWhatsApp and MobileStickyCTA

--- frontend/src/app/(main)/layout.js
+++ frontend/src/app/(main)/layout.js
@@ -1,19 +1,23 @@
 import Footer from "../components/layout/Footer";
+import MobileStickyCTA from "../components/layout/MobileStickyCTA";
 import Navbar from "../components/layout/Navbar";
+import FloatingWhatsApp from "../components/shared/FloatingWhatsApp";
 import ScrollToTop from "../components/shared/ScrollToTop";
 
-export default function MainLayout({children}) {
+export default function MainLayout({ children }) {
   return (
     <div className="flex min-h-dvh flex-col">
       <Navbar />
 
-      <main className="flex-1">
-        {children}
-      </main>
+      <main className="flex-1">{children}</main>
 
       <Footer />
 
+      <FloatingWhatsApp />
+
+      <MobileStickyCTA />
+
       <ScrollToTop />
     </div>
-  )
+  );
 }
